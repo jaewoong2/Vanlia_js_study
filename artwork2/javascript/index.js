fbc9b0ce4a148dbcda82b2f61d3adda34db44f34
jaewoong2 <jwisgenius@naver.com>
2021-09-24 02:46:54 +0900

    iamges

--- artwork2/javascript/index.js
+++ artwork2/javascript/index.js
@@ -4,7 +4,7 @@ const sleep = (ms) => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(true);
-    }, ms);
+    }, ms % 1000);
   });
 };
 
@@ -31,9 +31,13 @@ const content = {
   },
   fourth: {
     target: document.querySelector(".fourth"),
+    imageContainers: document
+      .querySelector(".fourth")
+      .querySelector(".image-containers"),
     imageContainer: document
       .querySelector(".fourth")
-      .querySelector(".image-container"),
+      .querySelectorAll(".image-container"),
+    years: document.querySelector(".years"),
   },
 };
 
@@ -102,13 +106,61 @@ const third = async () => {
   target.querySelector(".circle-box3").classList.add("circle-text3");
   target.querySelector(".circle-box2").classList.add("circle-text2");
   target.querySelector(".circle-box").classList.add("circle-text");
+  await sleep(2000);
   fourth();
 };
 
 const fourth = async () => {
-  const { target, imageContainer } = content.fourth;
-  target.style.height = `500%`;
-  imageContainer.classList.remove("hidden");
+  const { target, imageContainers, imageContainer, years } = content.fourth;
+  target.style.height = `1000%`;
+  target.classList.remove("hidden");
+
+  imageContainer.forEach((node, i) => {
+    if (i % 2) {
+      node.style.transform = `translateY(${50}px)`;
+    } else {
+      node.style.transform = `translateY(${-50}px)`;
+    }
+  });
+
+  let dx = 0;
+  let currentScroll = window.scrollY;
+
+  window.addEventListener("scroll", (e) => {
+    const { y, height } = target.getBoundingClientRect();
+    const { y: imageContainersY } = imageContainers.getBoundingClientRect();
+
+    if (y < 0) {
+      dx -= currentScroll - window.scrollY;
+      currentScroll = window.scrollY;
+
+      imageContainers.classList.add("fixed");
+      imageContainers.style.transform = `translateX(${-dx}px)`;
+
+      years.querySelectorAll("div").forEach((node, i) => {
+        if (-y < height * 0.2) {
+          i === 0 ? node.classList.add("now") : node.classList.remove("now");
+        } else if (-y < height * 0.4) {
+          i === 1 ? node.classList.add("now") : node.classList.remove("now");
+        } else if (-y < height * 0.6) {
+          i === 2 ? node.classList.add("now") : node.classList.remove("now");
+        } else if (-y < height * 0.8) {
+          i === 3 ? node.classList.add("now") : node.classList.remove("now");
+        } else if (-y < height) {
+          i === 4 ? node.classList.add("now") : node.classList.remove("now");
+        }
+      });
+
+      years.classList.add("fixed-years");
+    } else {
+      if (imageContainersY < 0) {
+        dx -= currentScroll - window.scrollY;
+      }
+      currentScroll = window.scrollY;
+      years.classList.remove("fixed-years");
+      imageContainers.classList.remove("fixed");
+    }
+  });
 };
 
 window.onload = init;
